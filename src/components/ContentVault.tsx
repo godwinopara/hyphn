1c65a215d348b5bf5f619fe47afc69bb7310a50e
Godwin Opara <godwinopara62@yahoo.com>
2023-11-10 11:58:35 +0100

    added call to action button that directs user to the content vault page

--- src/components/ContentVault.tsx
+++ src/components/ContentVault.tsx
@@ -3,62 +3,45 @@ import tv from "public/assets/tv-section.png";
 import event from "public/assets/event-section.png";
 import digital from "public/assets/digital-section.png";
 import Image from "next/image";
+import ContentVaultCard from "./ContentVaultCard";
+import arrow from "public/assets/arrow-white.svg";
 
 export default function ContentVault() {
 	return (
 		<section className="bg-black border border-black py-16">
 			<div className="max-w-desktop mx-auto">
-				<div>
-					<h2 className="text-yellow">CONTENT VAULT</h2>
-					<p>Take a sneak pick of what we have</p>
+				<div className="mb-6">
+					<h2 className="text-yellow font-medium text-xl mb-6">CONTENT VAULT</h2>
+					<p className="text-white1 text-3xl font-semibold">Take a sneak pick of what we have</p>
 				</div>
 				<div className="grid grid-cols-4 gap-8 bg-black">
-					<div>
-						<Image
-							src={radio}
-							alt="an image that represents a radio"
-							className="h-60 object-cover"
-						/>
-						<div className="px-10 py-8 bg-white h-44">
-							<span className="block mb-2 text-gray3 leading-130">RADIO</span>
-							<h3 className="leading-130 text-xl font-bold mb-2">World of Hero</h3>
-							<span className="text-sm leading-130">16 July 2023</span>
-						</div>
-					</div>
-					<div>
-						<div>
-							<Image src={tv} alt="an image that represents tv" className="h-60 object-cover" />
-						</div>
-						<div className="px-10 py-8 bg-white h-44">
-							<span className="text-gray3 leading-130 mb-2">TV</span>
-							<h3 className="leading-130 text-xl font-bold mb-2">Chasing Weddings</h3>
-							<span>20 November 2023</span>
-						</div>
-					</div>
-					<div>
-						<Image
-							src={digital}
-							alt="an image that represents a radio"
-							className="h-60 object-cover"
-						/>
-						<div className="px-10 py-8 bg-white h-44">
-							<span className="text-gray3 leading-130 mb-2">DIGITAL/ONLINE</span>
-							<h3 className="leading-130 text-xl font-bold mb-2">Ultimate Football Challenge</h3>
-							<span>O1 November 2023</span>
-						</div>
-					</div>
-					<div>
-						<Image
-							src={event}
-							alt="an image that represents a radio"
-							className="h-60 object-cover"
-						/>
-						<div className="px-10 py-8 bg-white h-44">
-							<span className="text-gray3 leading-130 mb-2">EVENTS</span>
-							<h3 className="leading-130 text-xl font-bold mb-2">Hero Music Album</h3>
-							<span>20 May 2023</span>
-						</div>
-					</div>
+					<ContentVaultCard img={radio} section="RADIO" title="World of Hero" date="16 July 2023" />
+
+					<ContentVaultCard
+						img={tv}
+						section="TV"
+						title="Chasing Weddings"
+						date="20 November 2023"
+					/>
+					<ContentVaultCard
+						img={digital}
+						section="DIGITAL/ONLINE"
+						title="Ultimate Football Challenge"
+						date="O1 November 2023"
+					/>
+
+					<ContentVaultCard
+						img={event}
+						section="EVENT"
+						title="Hero Music Album"
+						date="20 May 2023"
+					/>
+				</div>
+
+				<div className="text-white flex justify-center mt-10">
+					<button className="flex items-center justify-center gap-3 py-4 px-5 hover:bg-yellow rounded-lg">
+						Learn More <Image src={arrow} alt="arrow" />
+					</button>
 				</div>
 			</div>
 		</section>
---
+++ src/components/ContentVaultCard.tsx
@@ -0,0 +1,21 @@
+import Image, { StaticImageData } from "next/image";
+
+interface ContentVaultCardProps {
+	img: StaticImageData | string;
+	section: string;
+	title: string;
+	date: string;
+}
+
+export default function ContentVaultCard({ img, section, title, date }: ContentVaultCardProps) {
+	return (
+		<div>
+			<Image src={img} alt="an image that represents a radio" className="h-60 object-cover" />
+			<div className="px-10 py-8 bg-white h-44">
+				<span className="block mb-2 text-gray3 leading-130">{section}</span>
+				<h3 className="leading-130 text-xl font-bold mb-2">{title}</h3>
+				<span className="text-sm leading-130">{date}</span>
+			</div>
+		</div>
+	);
+}

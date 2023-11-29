2ac013a1029c6efb7b01507a483f3cc92860c53e
Godwin Opara <godwinopara62@yahoo.com>
2023-11-29 01:19:47 +0100

    fixed desktop layout to new design

--- src/components/TeamList.tsx
+++ src/components/TeamList.tsx
@@ -1,63 +1,45 @@
 import Image from "next/image";
-import huston1 from "public/assets/huston1.png";
-import huston2 from "public/assets/huston2.png";
-import huston3 from "public/assets/huston3.png";
-import facebook from "public/assets/facebook-color.svg";
-import instagram from "public/assets/instagram-color.svg";
-import linkedin from "public/assets/linkedin-color.svg";
+import lanre from "public/assets/lanre.png";
+import ada from "public/assets/ada.png";
+import blessing from "public/assets/blessing.png";
+import uche from "public/assets/Uche.png";
 
 export default function TeamList() {
 	return (
-		<div className="grid md:grid-cols-3 gap-16 text-center">
+		<div className="grid md:grid-cols-4 gap-16 text-center">
 			<div>
-				<Image src={huston1} alt="guy on black shirt sitting down" />
+				<Image src={ada} alt="guy on black shirt sitting down" />
 				<div className="mt-4">
-					<h3 className="font-bold text-xl mb-2">Ramandar Huston</h3>
-					<span className="mb-2 block">UI/UX desinger</span>
+					<h3 className="font-bold text-xl mb-2">Eze-nwoko Adaeze Confidence</h3>
+					<span className="mb-2 block">HR Generalist</span>
 					<p className="text-sm mb-4 w-4/5 mx-auto">
-						Currently working on a bombshell, amazing idea. works with Canva, Upwork as Developer
-						and IT designer.
+						Recruitment, employee relations, onboarding, compensation and benefits, performance
+						management, learning and development, and HR Administration
 					</p>
-
-					<div className="max-w-desktop mx-auto flex justify-center">
-						<Image src={facebook} alt="facebook " className="mr-2" />
-						<Image src={instagram} alt="facebook " className="mr-2" />
-						<Image src={linkedin} alt="facebook " className="mr-2" />
-					</div>
 				</div>
 			</div>
 			<div>
-				<Image src={huston2} alt="guy on black shirt sitting down" />
+				<Image src={lanre} alt="guy on black shirt sitting down" />
 				<div className="mt-4">
-					<h3 className="font-bold text-xl mb-2">Ramandar Huston</h3>
-					<span className="mb-2 block">UI/UX desinger</span>
-					<p className="text-sm mb-4 w-4/5 mx-auto">
-						Currently working on a bombshell, amazing idea. works with Canva, Upwork as Developer
-						and IT designer.
-					</p>
-
-					<div className="max-w-desktop mx-auto flex justify-center">
-						<Image src={facebook} alt="facebook " className="mr-2" />
-						<Image src={instagram} alt="facebook " className="mr-2" />
-						<Image src={linkedin} alt="facebook " className="mr-2" />
-					</div>
+					<h3 className="font-bold text-xl mb-2">Akinyele Olanrewaju Quadri</h3>
+					<span className="mb-2 block">Creative Manager</span>
+					<p className="text-sm mb-4 w-4/5 mx-auto">Graphics/ MoGraph/ Video Editing</p>
 				</div>
 			</div>
 			<div>
-				<Image src={huston3} alt="guy on black shirt sitting down" />
+				<Image src={blessing} alt="guy on black shirt sitting down" />
 				<div className="mt-4">
-					<h3 className="font-bold text-xl mb-2">Ramandar Huston</h3>
-					<span className="mb-2 block">UI/UX desinger</span>
-					<p className="text-sm mb-4 w-4/5 mx-auto">
-						Currently working on a bombshell, amazing idea. works with Canva, Upwork as Developer
-						and IT designer.
-					</p>
-
-					<div className="max-w-desktop mx-auto flex justify-center">
-						<Image src={facebook} alt="facebook " className="mr-2" />
-						<Image src={instagram} alt="facebook " className="mr-2" />
-						<Image src={linkedin} alt="facebook " className="mr-2" />
-					</div>
+					<h3 className="font-bold text-xl mb-2">Nwaigba Ngozi</h3>
+					<span className="mb-2 block">Client service executive</span>
+					<p className="text-sm mb-4 w-4/5 mx-auto">Organization, Scheduling, Customer Service.</p>
+				</div>
+			</div>
+			<div>
+				<Image src={uche} alt="guy on black shirt sitting down" />
+				<div className="mt-4">
+					<h3 className="font-bold text-xl mb-2">Ebenyi Uchechukwu</h3>
+					<span className="mb-2 block">Office Assitant</span>
+					<p className="text-sm mb-4 w-4/5 mx-auto">Organization, Scheduling, Logistics</p>
 				</div>
 			</div>
 		</div>
